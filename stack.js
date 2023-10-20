8714ab0286aacb44c271788cd4726843daf25bf5
Enry Vardanyan <Vardanyan.enry7@gmail.com>
2023-10-20 13:34:31 -0700

    push fixed isEmpty added

--- stack.js
+++ stack.js
@@ -1,7 +1,7 @@
 class Node {
     constructor(data){
         this.data = data;
-        this.next = next 
+        this.next = null 
     }
 }
 class Stack {
@@ -9,8 +9,18 @@ class Stack {
         this.top = null
     }
     push(node){
+        if(this.isEmpty()) {
+            this.top = node
+            return this
+        }
         this.top.next = this.top
         this.top = node 
     }
-    
+    isEmpty(){
+        return !this.top
+    }
 }
+const node1 = new Node(10)
+const stack = new Stack()
+stack.push(node1)
+console.log(stack)
