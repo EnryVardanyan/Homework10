609949a9724e42dd1e6e12cf9ce7f3aea09ef104
Enry Vardanyan <Vardanyan.enry7@gmail.com>
2023-10-20 13:28:34 -0700

    Stack and Node created push method added

---
+++ stack.js
@@ -0,0 +1,16 @@
+class Node {
+    constructor(data){
+        this.data = data;
+        this.next = next 
+    }
+}
+class Stack {
+    constructor(){
+        this.top = null
+    }
+    push(node){
+        this.top.next = this.top
+        this.top = node 
+    }
+    
+}
