cb89de598cd50fa42e25628960efec61416e0dba
Enry Vardanyan <Vardanyan.enry7@gmail.com>
2023-10-20 18:11:56 -0700

    dequeue added

--- queue.js
+++ queue.js
@@ -3,6 +3,17 @@ class Queue {
         this.items = []
     }
     enqueue(data){
-        this.array.push(data)
+        this.items.push(data)
+    }
+    dequeue(){
+        if(!this.items.length) return null
+        const sliced = this.items[0]
+        this.items.shift()
+        return sliced
     }
 }
+const queue = new Queue()
+queue.enqueue(6)
+queue.enqueue(7)
+queue.dequeue()
+console.log(queue)
