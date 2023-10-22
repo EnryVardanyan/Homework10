02e59ded599016207ff43e38603856fcb6fc7e99
Enry Vardanyan <Vardanyan.enry7@gmail.com>
2023-10-21 18:43:01 -0700

    getUserById method added

--- mappedUser.js
+++ mappedUser.js
@@ -1,10 +1,20 @@
 class Users{
     constructor(data){
         this.maped = new Map()
-        data.forEach.addUser(user => this.addUser(user))
+        data.forEach(user => this.addUser(user))
     }
     addUser(user){
         this.maped.set(user.id, user)
     }
+    getUserById(id){
+       return this.maped.get(id)
+    }
 
 }
+const mappedUser = new Users([ 
+    { id: 1, name: "Alice", email: "alice@example.com" }, 
+    { id: 2, name: "Bob", email: "bob@example.com" }, 
+  ])
+  console.log(mappedUser.getUserById(3))
+  mappedUser.addUser({id: 3, name: "Hakobik", email: "hakobik@rambler.ru"})
+  console.log(mappedUser)
